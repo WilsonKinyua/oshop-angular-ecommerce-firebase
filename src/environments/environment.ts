89a6180ed8218d6451f676d578119efeb190c2ee
wilson <wilsonkinyuam@gmail.com>
2020-09-16 02:42:55 +0300

    working routermodule and ng-bootstrap dropdown

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBpgjtjyoCzSCff3H2E5v4t3DHgPQd-ogk",
+    authDomain: "oshop-store-fef5b.firebaseapp.com",
+    databaseURL: "https://oshop-store-fef5b.firebaseio.com",
+    projectId: "oshop-store-fef5b",
+    storageBucket: "oshop-store-fef5b.appspot.com",
+    messagingSenderId: "277107744315",
+    appId: "1:277107744315:web:f82f30deb25750b118861e",
+    measurementId: "G-RX2DGNV42S"
+  }
 };
 
 /*
